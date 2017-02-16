303016cbf8c530904364c77d60fa7a01c1dd27d8
Rene Kreupl <info@renekreupl.de>
2017-02-16 22:26:32 +0100

    [TASK] Add runSequence for the build Task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,9 +2,13 @@ var gulp			= require('gulp');
 var notify          = require('gulp-notify');
 var modernizr 		= require('gulp-modernizr');
 var uglify 			= require('gulp-uglify');
+var runSequence 	= require('run-sequence');
 var config 			= require('../config');
 
-gulp.task('build', config.build, function() {
+
+gulp.task('build', function() {
+
+	runSequence.apply(null, config.build);
 
 	// build modernizr
 	gulp.src(config.js.src)
