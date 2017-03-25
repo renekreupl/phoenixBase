e2788fb9e60d8e2b24aa7460aa96a7b029fb75b1
Rene Kreupl <info@renekreupl.de>
2017-03-25 20:40:56 +0100

    [TASK] Delete copy of some vendor css files

--- gulp/tasks/vendor.js
+++ gulp/tasks/vendor.js
@@ -5,17 +5,6 @@ var config 			= require('../config').vendor;
 
 gulp.task('vendor', function() {
 
-	//Copy normalize.css and rename it to a scss file
-	gulp.src('./node_modules/normalize.css/normalize.css')
-		.pipe(rename("_normalize.scss"))
-		.pipe(gulp.dest( config.paths.sass))
-		.pipe(notify({ message: 'Copy normalize.css successful' }));
-
-	//Copy _include-media.scss
-	gulp.src('./node_modules/include-media/dist/_include-media.scss')
-		.pipe(gulp.dest( config.paths.sass))
-		.pipe(notify({ message: 'Copy _include-media.scss successful' }));
-
 	//Copy JS Files from Config File
 	gulp.src(config.js)
 		.pipe(gulp.dest(config.paths.js))
