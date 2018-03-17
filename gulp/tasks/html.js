aa744f9b62e9d3ad286bf1f64f4d69fafa40cf66
Rene Kreupl <info@renekreupl.de>
2018-03-17 05:48:57 +0100

    [BUGFIX] fix decleration

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -6,7 +6,7 @@ gulp.task('html', function() {
 	return gulp.src( config.src+'/*.html' )
 		.pipe(use.twig())
 		.pipe(use.htmlmin({collapseWhitespace: true}))
-		.pipe(use.plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+		.pipe(use.plumber({errorHandler: use.notify.onError("Error: <%= error.message %>")}))
 		.pipe(gulp.dest( config.build ))
 		.pipe(use.notify({ message: 'Yo, HTML task complete.' }));
 });
